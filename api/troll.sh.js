49f1765b463dcd5b5ab27c80126ca7577c2419c7
Tiago Antunes Bugalho <antunesbugalhot@gmail.com>
2026-08-19 18:08:44 -0300

    aaaa

--- api/troll.sh.js
+++ api/troll.sh.js
@@ -2,5 +2,5 @@ export default function handler(request, response) {
   response.setHeader('Content-Type', 'text/plain; charset=utf-8')
   response.setHeader('Cache-Control', 'no-store')
 
-  return response.status(200).send('#!/usr/bin/env bash\necho foi trolado\n')
+  return response.status(200).send('echo foi trolado\n')
 }
